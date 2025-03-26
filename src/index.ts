856a401b5fcf8239941869de943b9412df27dac9
raghava20 <raghavaanand22@gmail.com>
2025-03-26 09:17:44 +0530

    fix:cors

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
